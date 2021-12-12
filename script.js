c7e522e99f1430a6653b8b6a6d6f64c3c83f9c2c
Thayna Rodrigues <thna.rdg@gmail.com>
2021-12-12 11:31:04 -0300

    UI Assets

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function playRound(){    
+function playRound(){
+        
     const playerOptions = prompt("Rock, Paper or Scissors?");
     let playerSelection = playerOptions.toLowerCase();
     console.log(playerSelection);
@@ -7,9 +8,10 @@ function playRound(){
     function computerPlay() {
         return computerOptions[Math.floor(Math.random()*computerOptions.length)];
     }
+
     let computerSelection = computerPlay();
     console.log(computerSelection);
-
+    
     let win = "You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();
     let lose = "You lose! " + computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase();
 
@@ -18,31 +20,64 @@ function playRound(){
     }
 
     else if (playerSelection === "rock") {
-        if (computerSelection === "scissors")
+        if (computerSelection === "scissors") {
+            playerScore++;
             return win;
-        
+        }
+
         else {
+            computerScore++;
             return lose;
         }
     }
 
     else if (playerSelection === "paper") {
-        if (computerSelection === "rock")
+        if (computerSelection === "rock") {
+            playerScore++;
             return win;
+        }
 
         else {
+            computerScore++;
             return lose;
         }
     }
 
     else if (playerSelection === "scissors") {
-        if (computerSelection === "paper")
+        if (computerSelection === "paper") {
+            playerScore++;
             return win;
+        }
 
         else {
+            computerScore++;
             return lose;
         }
     }
+
 }
 
-console.log(playRound());
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+
+function game() {
+    for(i=0; i<5; i++) {
+    console.log(playRound());
+    console.log("Your Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    }
+
+    if(playerScore === computerScore) {
+        return "It's a tie!" + playerScore + " vs " + computerScore;
+    }
+
+    else if (playerScore > computerScore) {
+        return "Player Wins! Your Score: " + playerScore;
+    }
+
+    else {
+        return "Computer Wins! Your Score: " + playerScore;
+    }
+}
+
+alert(game());
