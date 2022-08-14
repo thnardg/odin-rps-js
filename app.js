e7449f6c3c14ae40f775bab1ed921df9e5acc974
Thayna Rodrigues <thna.rdg@gmail.com>
2022-08-13 22:21:36 -0300

    Adds color to console.logs

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+// CONSOLE:
+const newGameMsg = "\x1b[35m A new game has started! \x1b[0m";
+const winMsg = "\x1b[32m You won this round! \x1b[0m";
+const tieMsg = "\x1b[36m It's a tie! \x1b[0m";
+const loseMsg = "\x1b[31m Computer won this round! \x1b[0m";
+
+// INTERFACE:
 const rock = document.getElementById("rock-btn");
 const paper = document.getElementById("paper-btn");
 const scissors = document.getElementById("scissors-btn");
-const restart = document.getElementById("restart-btn");
+const restartGame = document.getElementById("restart-btn");
 
 const displayUser = document.getElementById("user-display");
 const displayPc = document.getElementById("pc-display");
@@ -9,21 +16,79 @@ const displayPc = document.getElementById("pc-display");
 const userScore = document.getElementById("user-score");
 const pcScore = document.getElementById("pc-score");
 
+const resultMessage = document.getElementById("result-msg");
+
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+let round = parseInt(0);
+
 window.onload = newGameSetup();
 
-restart.addEventListener("click", () => {
+restartGame.addEventListener("click", () => {
   newGameSetup();
 });
 
 /* SETUP NEW GAME - Clean scores and display */
 function newGameSetup() {
-  let playerScore = parseInt(0);
-  let computerScore = parseInt(0);
-  let round = parseInt(0);
-  console.log("\x1b[36m%s\x1b[0m", "A new game has started!");
+  playerScore = parseInt(0);
+  computerScore = parseInt(0);
+  round = parseInt(0);
+  console.log(newGameMsg);
+  console.log(tieMsg);
+  console.log(winMsg);
+  console.log(loseMsg);
+
   //TODO change display class to hide image inside
 }
 
 function countRounds() {
   round += 1;
+  return round;
+}
+
+function computerPlay() {
+  const computerOptions = ["rock", "paper", "scissors"];
+  return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+
+function playRound() {
+  // GET PLAYER CHOICE BY CLICKING BUTTON - STORE IT - DISPLAY IT
+
+  // GET COMPUTER CHOICE BY SELECTING A RANDOM VALUE - STORE IT - DISPLAY IT
+  let computerSelection = computerPlay();
+
+  // SCORE POINTS
+  if (playerSelection === computerSelection) {
+    //DISPLAY TIE
+    console.log(tieMsg);
+    return;
+  } else if (playerSelection === rock) {
+    if (computerSelection === computerOptions[2]) {
+      playerScore++;
+      console.log(winMsg);
+      return win;
+    } else {
+      computerScore++;
+      return lose;
+    }
+  } else if (playerSelection === paper) {
+    if (computerSelection === computerOptions[0]) {
+      playerScore++;
+      console.log(winMsg);
+      return win;
+    } else {
+      computerScore++;
+      return lose;
+    }
+  } else if (playerSelection === scissors) {
+    if (computerSelection === computerOptions[1]) {
+      playerScore++;
+      console.log(winMsg);
+      return win;
+    } else {
+      computerScore++;
+      console.log(loseMsg);
+      return lose;
+    }
+  }
 }
--- script.js
+++ script.js
@@ -1,65 +1,59 @@
 // Function for Each Round:
-function playRound(){
-    
-    // Get player option, make it lowercase, and store it in a variable:
-    const playerOptions = //prompt("Rock, Paper or Scissors?");
-    let playerSelection = playerOptions.toLowerCase();
-    console.log(playerSelection);
+function playRound() {
+  // Get player option, make it lowercase, and store it in a variable:
+  const playerOptions = prompt("Rock, Paper or Scissors?");
+  let playerSelection = playerOptions.toLowerCase();
+  console.log(playerSelection);
 
-    // Get computer option by selection a random value from an array:
-    const computerOptions = ["rock", "paper", "scissors"];
-    function computerPlay() {
-        return computerOptions[Math.floor(Math.random()*computerOptions.length)];
+  // Get computer option by selection a random value from an array:
+  const computerOptions = ["rock", "paper", "scissors"];
+  function computerPlay() {
+    return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  }
+  // Make a variable for the computer option:
+  let computerSelection = computerPlay();
+  console.log(computerSelection);
+
+  // Win or Lose messages.
+  let win =
+    "You Win! " +
+    playerSelection.toUpperCase() +
+    " beats " +
+    computerSelection.toUpperCase();
+  let lose =
+    "You lose! " +
+    computerSelection.toUpperCase() +
+    " beats " +
+    playerSelection.toUpperCase();
+
+  // Every possible scenario for the player vs computer, and a score system:
+  if (playerSelection === computerSelection) {
+    return "It's a tie! ";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      playerScore++;
+      return win;
+    } else {
+      computerScore++;
+      return lose;
     }
-    // Make a variable for the computer option:
-    let computerSelection = computerPlay();
-    console.log(computerSelection);
-    
-    // Win or Lose messages.
-    let win = "You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();
-    let lose = "You lose! " + computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase();
-
-    // Every possible scenario for the player vs computer, and a score system:
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      playerScore++;
+      return win;
+    } else {
+      computerScore++;
+      return lose;
     }
-
-    else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            playerScore++;
-            return win;
-        }
-
-        else {
-            computerScore++;
-            return lose;
-        }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      playerScore++;
+      return win;
+    } else {
+      computerScore++;
+      return lose;
     }
-
-    else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            playerScore++;
-            return win;
-        }
-
-        else {
-            computerScore++;
-            return lose;
-        }
-    }
-
-    else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            playerScore++;
-            return win;
-        }
-
-        else {
-            computerScore++;
-            return lose;
-        }
-    }
-
+  }
 }
 
 let playerScore = parseInt(0);
@@ -67,23 +61,19 @@ let computerScore = parseInt(0);
 
 // LOOP for a game of 5 rounds:
 function game() {
-    for(i=0; i<5; i++) {
+  for (i = 0; i < 5; i++) {
     console.log(playRound());
     console.log("Your Score: " + playerScore);
     console.log("Computer Score: " + computerScore);
-    }
+  }
 
-    if(playerScore === computerScore) {
-        return "It's a tie!" + playerScore + " vs " + computerScore;
-    }
-
-    else if (playerScore > computerScore) {
-        return "Player Wins! Your Score: " + playerScore;
-    }
-
-    else {
-        return "Computer Wins! Your Score: " + playerScore;
-    }
+  if (playerScore === computerScore) {
+    return "It's a tie! " + playerScore + " vs " + computerScore;
+  } else if (playerScore > computerScore) {
+    return "Player Wins! Your Score: " + playerScore;
+  } else {
+    return "Computer Wins! Your Score: " + playerScore;
+  }
 }
 
 alert(game());
